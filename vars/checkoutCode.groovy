e4767cd684427a450236c7442c6a1c4c76800e36
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-03-31 22:06:28 +0700

    test checkout lib

---
+++ vars/checkoutCode.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+def call(Map stageParams) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [
+            [ url: stageParams.url ],
+            // [credentialsId: stageParams.credentialsId]
+        ]
+    ])
+}
