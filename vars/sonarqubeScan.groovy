61aa9d77490cf9430b6fa1cd6c307a1743f2573b
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-04-01 16:21:23 +0700

    fix sonar lib

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -1,15 +1,12 @@
 #!/usr/bin/bash groovy
-def call() {
-    environment {
-    scannerHome = tool 'SonarQubeScanner'
-    }
-    steps {
-        withSonarQubeEnv('T2P-SonarQube') {
-            sh "${sonar}/bin/sonar-scanner"
-        } // submitted SonarQube taskId is automatically attached to the pipeline context
-        timeout(time: 10, unit: 'MINUTES') {
-            waitForQualityGate abortPipeline: true
-        }
+def call(Map sonar) { 
+    def sonar.workspace = manager.build.getEnvVars()["WORKSPACE"]
+    def sonar.properties = ${sonar.workspace}'/'${sonar.file}
+    withSonarQubeEnv('T2P-SonarQube') {
+        sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${sonar.properties}"
+    } // submitted SonarQube taskId is automatically attached to the pipeline context
+    timeout(time: 10, unit: 'MINUTES') {
+        waitForQualityGate abortPipeline: true
     }
 }
 
