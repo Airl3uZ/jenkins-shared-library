23edd4549a7429ef9ba13f089c0dd8e1efa8f404
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-04-01 19:28:24 +0700

    get system.env

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -1,7 +1,10 @@
 #!/usr/bin/bash groovy
 def call(Map sonar) {
+    def env = System.getenv()
+    def workspace = env.['WORKSPACE']
+    sonar.projectFile = "${workspace}/${sonar.file}"
     withSonarQubeEnv('T2P-SonarQube') {
-        sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${sonar.properties}"
+        sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${sonar.projectFile}"
     } // submitted SonarQube taskId is automatically attached to the pipeline context
     timeout(time: 10, unit: 'MINUTES') {
         waitForQualityGate abortPipeline: true
