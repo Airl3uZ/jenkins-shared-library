cc8ac64aba5305861322083b49c42f009a9f1f7b
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-04-01 19:34:56 +0700

    string wok space = env.workspace

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/bash groovy
 def call(Map sonar) {
     def env = System.getenv()
-    def workspace = env.['WORKSPACE']
-    sonar.projectFile = "${workspace}/${sonar.file}"
+    String workspace = env.WORKSPACE
+    sonar.projectFile = "${workspace}"+'/'+"${sonar.file}"
     withSonarQubeEnv('T2P-SonarQube') {
         sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${sonar.projectFile}"
     } // submitted SonarQube taskId is automatically attached to the pipeline context
@@ -10,4 +10,3 @@ def call(Map sonar) {
         waitForQualityGate abortPipeline: true
     }
 }
-
