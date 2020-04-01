ad6333d425c4e3d09c77e53e403a190664e82f04
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-04-01 19:04:33 +0700

    create object for get env

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/bash groovy
 def call(Map sonar) { 
-    def workspace = get-env.WORKSPACE
-    def properties = ${workspace}'/'${sonar.file}
+    new workspace = get-env.WORKSPACE
+    new properties = ${workspace}'/'${sonar.file}
     withSonarQubeEnv('T2P-SonarQube') {
         sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${properties}"
     } // submitted SonarQube taskId is automatically attached to the pipeline context
