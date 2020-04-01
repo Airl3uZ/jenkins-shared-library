d2b1d0b28043c258ec2cd7971e51171915437576
Siriwat Jiwsuwan <Airl3uZ@t2p.corp>
2020-04-01 18:58:57 +0700

    fix error

--- vars/sonarqubeScan.groovy
+++ vars/sonarqubeScan.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/bash groovy
 def call(Map sonar) { 
-    def sonar.workspace = get-env.WORKSPACE
-    def sonar.properties = ${sonar.workspace}'/'${sonar.file}
+    def workspace = get-env.WORKSPACE
+    def properties = ${workspace}'/'${sonar.file}
     withSonarQubeEnv('T2P-SonarQube') {
-        sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${sonar.properties}"
+        sh "${sonar.home}/bin/sonar-scanner -Dproject.settings=${properties}"
     } // submitted SonarQube taskId is automatically attached to the pipeline context
     timeout(time: 10, unit: 'MINUTES') {
         waitForQualityGate abortPipeline: true
