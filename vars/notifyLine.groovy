92836edb39b5577532aba07b4ad16be5e76ff387
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-09 12:40:06 +0700

    check pipeline

--- vars/notifyLine.groovy
+++ vars/notifyLine.groovy
@@ -1,11 +1,15 @@
 #!groovy
-def call(stageResults) {
+def call(String stageResults = 'No result send from pipeline') {
     def token_t2p = "0vLiHpCXMxx5zKVYf9VdmQTRI61J250Eq2ozN3NW06M"
     def test_token = "BX9PfOsvLw8s1voSR8XqsSOyfKrRGVnxtSUpjvS6h6f"
     def jobName = env.JOB_NAME +' '+env.BRANCH_NAME
     def buildNo = env.BUILD_NUMBER
-if (jobName == 'testScipt') token = test_token
-else token = token_t2p
+    if (jobName == 'testScript') {
+        def token = test_token
+    }
+    else {
+        def token = token_t2p
+    }
 
     def url = 'https://notify-api.line.me/api/notify'
     def message = "${jobName} Build #${buildNo} ${stageResults} \r\n"
