213667254f1293b3c10b4be07a2e3adcbb7465c9
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-09 12:30:20 +0700

    lib notifyLine noti to my line acc

--- vars/notifyLine.groovy
+++ vars/notifyLine.groovy
@@ -1,11 +1,14 @@
 #!groovy
 def call(stageResults) {
-    def token = "0vLiHpCXMxx5zKVYf9VdmQTRI61J250Eq2ozN3NW06M"
+    def token_t2p = "0vLiHpCXMxx5zKVYf9VdmQTRI61J250Eq2ozN3NW06M"
+    def test_token = "BX9PfOsvLw8s1voSR8XqsSOyfKrRGVnxtSUpjvS6h6f"
     def jobName = env.JOB_NAME +' '+env.BRANCH_NAME
     def buildNo = env.BUILD_NUMBER
+if (jobName == 'testScipt') token = test_token
+else token = token_t2p
 
     def url = 'https://notify-api.line.me/api/notify'
-    def message = "${jobName} Build #${buildNo} ${stageResults.first} \r\n"
+    def message = "${jobName} Build #${buildNo} ${stageResults} \r\n"
     // sh "curl ${url} -H 'Authorization: Bearer ${token}' -F 'message=${message}'"
     echo "curl ${url} -H 'Authorization: Bearer ${token}' -F 'message=${message}'"
 }
