44bf36bea0348646b8e8493b8dbddf38841ca6ad
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-10 12:19:03 +0700

    split yaml

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,5 +1,6 @@
 #!groovy
 def call(String yaml = "config.yml") {
-    Map cfg = readYaml(file: "${yaml}")
-    return cfg
+    Map cfg = readYaml(file: "${yaml}".config)
+    Map app_cfg = readYaml(file: "${yaml}".environment)
+    return cfg, app_cfg
 }
