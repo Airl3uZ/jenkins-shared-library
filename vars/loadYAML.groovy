54ecb34c59203bdbc06f7504a7d0ad6928353336
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-10 17:45:23 +0700

    use snakeyaml

--- vars/loadYAML.groovy
+++ vars/loadYAML.groovy
@@ -3,8 +3,13 @@
 // import groovy.grape.Grape
 // grape.grab(group='org.codehaus.groovy', module='groovy-all', version='2.5.11')
 import groovy.yaml.YamlBuilder
+import org.yaml.snakeyaml.Yaml
+import hudson.model.*
 def call(String file = 'ci/config.yml') {
-    def parser = new Yaml()
-    def example = parser.load(file.text)
-    println example
+    // def parser = new Yaml()
+    // def example = parser.load(file.text)
+    // println example
+    Yaml parser = new Yaml()
+    servers=parser.load(("${file}" as File).text)
+    println servers
 }
