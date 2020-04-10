88624ea05bd8433b2e89ff00ed1648c7a78d73ad
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-10 17:31:05 +0700

    import class

--- vars/loadYAML.groovy
+++ vars/loadYAML.groovy
@@ -2,7 +2,7 @@
 // @grab(group='org.codehaus.groovy', module='groovy-all', version='2.5.11')
 // import groovy.grape.Grape
 // grape.grab(group='org.codehaus.groovy', module='groovy-all', version='2.5.11')
-// import groovy.yaml.YamlBuilder
+import groovy.yaml.YamlBuilder
 def call(String file = 'ci/config.yml') {
     def parser = new Yaml()
     def example = parser.load(file.text)
