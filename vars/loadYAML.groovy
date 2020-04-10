6c2eb8998bc23b977265bb9f1bcd3ee681e468a6
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-10 18:05:29 +0700

    add node

--- vars/loadYAML.groovy
+++ vars/loadYAML.groovy
@@ -5,11 +5,17 @@
 import org.yaml.snakeyaml.Yaml
 import hudson.model.*
 def call(String file = 'ci/config.yml') {
+    def config = new File("${file}")
+node
+{
+    stage("Populating Environment")
+    {
     // def parser = new Yaml()
     // def example = parser.load(file.text)
     // println example
-    def config = new File("${file}")
     Yaml parser = new Yaml()
     servers=parser.load(("${config}" as File).text)
     println servers
+}
+    }
 }
