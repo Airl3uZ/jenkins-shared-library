874622f037f87c47b6281dff7477461f4ffcd66a
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-13 13:34:05 +0700

    test dynamic extension

--- vars/githubCheckout.groovy
+++ vars/githubCheckout.groovy
@@ -4,6 +4,7 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
+        extensions: [[stageParams.extensions ]],
         extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: stageParams.path ]],
         userRemoteConfigs: [[ url:  "https://github.com/"+stageParams.user+"/"+stageParams.repo ]]
     ])
