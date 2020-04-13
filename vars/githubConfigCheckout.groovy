0415e95827604ce04e0dbe2ce5e7bb701caa1b38
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-13 13:41:18 +0700

    add githubConfigCheckout

--- vars/githubConfigCheckout.groovy
+++ vars/githubConfigCheckout.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 def call(Map stageParams) {
+    def scmExtension = [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'ci']],          [$class: 'RelativeTargetDirectory', relativeTargetDir: 'ci' ]]]
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
-        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: stageParams.path ]],
+        extensions: scmExtension,
         userRemoteConfigs: [[ url:  "https://github.com/"+stageParams.user+"/"+stageParams.repo ]]
     ])
 }
