275d3b54ad16d2dd78a847e0127e9c8a5f595f1f
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-13 13:44:03 +0700

    fix []

--- vars/githubConfigCheckout.groovy
+++ vars/githubConfigCheckout.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 def call(Map stageParams) {
-    def scmExtension = [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'ci']],          [$class: 'RelativeTargetDirectory', relativeTargetDir: 'ci' ]]]
+    def scmExtension = [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'ci']]], [$class: 'RelativeTargetDirectory', relativeTargetDir: 'ci' ]]
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
